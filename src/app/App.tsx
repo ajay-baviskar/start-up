23abf3687a57a23f8b52cbdfe0e874f81d2061ba
Ajay <ajay.baviskar88@gmail.com>
2026-01-31 08:14:58 +0530

    comment testimonial

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
         <Process />
         <About />
         {/* <Team /> */}
-        <Testimonials />
+        {/* <Testimonials /> */}
         <ComingSoon />
         <Careers />
         <Contact />
